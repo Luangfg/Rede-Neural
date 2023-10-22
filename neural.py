89358ac4b37a561ec9db58c9cb7489b926eba43d
Luangfg <luangoncalvesfernandes@gmail.com>
2023-10-22 13:57:04 -0300

    Initial commit

---
+++ neural.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+transform = transforms.ToTensor()
+
+trainset = datasets.MNIST('./MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+valset = datasets.MNIST('./MNIST_data/', download=True, train=False, transform=transform)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
